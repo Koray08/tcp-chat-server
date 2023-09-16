124f5e646cd7fbef4ef372ca0cc33068cab2e7e6
Koray08 <koray.testing@gmail.com>
2023-09-16 17:49:05 +0300

    Error handling for nicknames

--- client.c
+++ client.c
@@ -7,6 +7,10 @@
 
 char msg[500];
 
+int isNicknameTooLong(const char *nickname) {
+    return strlen(nickname) > 100;
+}
+
 void *recvmg(void *my_sock) {
     int sock = *((int *)my_sock);
     int len;
@@ -17,30 +21,46 @@ void *recvmg(void *my_sock) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./client <nickname>\n");
+        return 1;
+    }
+
+    char *client_name = argv[1];
+
+    if (isNicknameTooLong(client_name)) {
+        printf("Nickname should be shorter (max 100 characters).\n");
+        return 1;
+    }
+
     pthread_t recvt;
     int len;
     int sock;
     struct sockaddr_in ServerIp;
-    char client_name[100];
-    strcpy(client_name, argv[1]);
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
     ServerIp.sin_port = htons(1234);
     ServerIp.sin_family = AF_INET;
     ServerIp.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if ((connect(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp))) == -1)
-        printf("\n connection to socket failed \n");
+
+    if ((connect(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp))) == -1) {
+        printf("\nConnection to socket failed\n");
+        return 1;
+    }
 
     pthread_create(&recvt, NULL, (void *)recvmg, &sock);
 
     len = write(sock, client_name, strlen(client_name));
     if (len < 0) {
-        printf("\n message not sent \n");
+        printf("\nMessage not sent\n");
+        return 1;
     }
 
     while (fgets(msg, 500, stdin) > 0) {
         len = write(sock, msg, strlen(msg));
         if (len < 0) {
-            printf("\n message not sent \n");
+            printf("\nMessage not sent\n");
+            return 1;
         }
     }
 
@@ -50,7 +70,8 @@ int main(int argc, char *argv[]) {
     sprintf(disconnect_msg, "%s disconnected", client_name);
     len = write(sock, disconnect_msg, strlen(disconnect_msg));
     if (len < 0) {
-        printf("\n message not sent \n");
+        printf("\nMessage not sent\n");
+        return 1;
     }
 
     shutdown(sock, SHUT_RDWR);
